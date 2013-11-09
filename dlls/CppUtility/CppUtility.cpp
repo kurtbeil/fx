46ba8c33f32718a6d8a65dd0af1608eb65d5749a
xmduhan <xmduhan@gmail.com>
2013-11-09 10:12:23 +0800

    增加死锁问题确认代码

--- dlls/CppUtility/CppUtility.cpp
+++ dlls/CppUtility/CppUtility.cpp
@@ -12,7 +12,7 @@ CRITICAL_SECTION _StringParameter;
 CRITICAL_SECTION _GenerateExecuteId;
 
 // 限价单队列的事务锁
-CRITICAL_SECTION _LimitOrderQueen;
+CRITICAL_SECTION _LimitOrderQueue;
 
 
 BOOL APIENTRY DllMain( HANDLE hModule, 
@@ -22,7 +22,7 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 {
 	::InitializeCriticalSection(&_StringParameter);
 	::InitializeCriticalSection(&_GenerateExecuteId);
-	::InitializeCriticalSection(&_LimitOrderQueen);
+	::InitializeCriticalSection(&_LimitOrderQueue);
 	switch (ul_reason_for_call)
 	{
 		case DLL_PROCESS_ATTACH:
@@ -33,6 +33,50 @@ BOOL APIENTRY DllMain( HANDLE hModule,
     }
     return TRUE;
 }
+
+/*----------------------------------------------
+--                输出日志过程                --
+-----------------------------------------------*/
+
+
+void FormatTime(time_t t, char *strTime)
+{
+	struct tm tm1;
+	tm1 = *localtime(&t);
+    sprintf(
+		strTime,"%4.4d-%2.2d-%2.2d %2.2d:%2.2d:%2.2d",
+        tm1.tm_year+1900, tm1.tm_mon+1, tm1.tm_mday,
+        tm1.tm_hour, tm1.tm_min,tm1.tm_sec
+	);
+}
+
+void WriteLog(char * msg){
+	FILE *fp;
+	char strTime[1024];
+	time_t t = time(NULL);
+	FormatTime(t,strTime);
+    if (fp=fopen("c:\\CppUtility.log","a+")){
+		fprintf(fp,"%s:%s\n",strTime,msg);
+		fclose(fp);
+	}
+}
+
+/*----------------------------------------------
+--             自定义的关键区进出过程         --
+-----------------------------------------------*/
+
+void LockCS(CRITICAL_SECTION * cs){
+	if(!TryEnterCriticalSection(cs)){
+		// 执行到这里显然还是会将当前进程锁死
+		// 我们只是希望在此之前记录下一些信息确定死锁确实发生在此处
+		WriteLog("发生死锁");
+		EnterCriticalSection(cs);
+	}
+}
+
+void UnlockCS(CRITICAL_SECTION * cs){
+	LeaveCriticalSection(cs);
+}
  
 /*----------------------------------------------
 --              生成ExecuteId功能             --
@@ -40,12 +84,12 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 
 MT4_EXPFUNC int __stdcall GenerateExecuteId(){	
 	time_t t;
-	::EnterCriticalSection(&_GenerateExecuteId); // 锁定函数GenerateExecuteId函数
+	LockCS(&_GenerateExecuteId); // 锁定函数GenerateExecuteId函数
 	try{
 		t = time(NULL);
 		Sleep(1000);    // 保证下一个线程读取ExecuteId的时间是下一秒，这样不会发生重复
 	}catch(...){}
-	::LeaveCriticalSection(&_GenerateExecuteId); // 释放函数GenerateExecuteId函数
+	UnlockCS(&_GenerateExecuteId); // 释放函数GenerateExecuteId函数
 	return (t);	
 }
 
@@ -58,7 +102,7 @@ map<int,map<string,string> > StringParameter;
 
 
 MT4_EXPFUNC void __stdcall GlobalStringSet(int ExecuteId,char * name,char * value){
-	::EnterCriticalSection(&_StringParameter); // 锁定变量StringParameter
+	LockCS(&_StringParameter); // 锁定变量StringParameter
 	try{
 		// 对应的ExecuteId是否已经建立起全局变量集
 		if ( StringParameter.count(ExecuteId) <= 0){
@@ -67,14 +111,14 @@ MT4_EXPFUNC void __stdcall GlobalStringSet(int ExecuteId,char * name,char * valu
 		}
 		StringParameter[ExecuteId][name] = value;  //   这里赋值实际上是通过value先创造出一个string
 	}catch(...){}
-	::LeaveCriticalSection(&_StringParameter); // 释放变量StringParameter	 
+	UnlockCS(&_StringParameter); // 释放变量StringParameter	 
 }
 
 
 MT4_EXPFUNC char* __stdcall GlobalStringGet(int ExecuteId,char * name){
 	//static char buf[512];
 	char * result = NULL;
-	::EnterCriticalSection(&_StringParameter); // 锁定变量StringParameter
+	LockCS(&_StringParameter); // 锁定变量StringParameter
 	try{
 		if ( StringParameter.count(ExecuteId) > 0){
 			map<string,string> p = StringParameter[ExecuteId];			
@@ -88,7 +132,7 @@ MT4_EXPFUNC char* __stdcall GlobalStringGet(int ExecuteId,char * name){
 			result = "";
 		}
 	}catch(...){}
-	::LeaveCriticalSection(&_StringParameter); // 释放变量StringParameter 	
+	UnlockCS(&_StringParameter); // 释放变量StringParameter 	
 	return result;
 }
 
@@ -111,16 +155,16 @@ struct LimitOrder{
 int LimitOrderIdSeq = 1;
 
 // 存储限价单
-map<int,queue<LimitOrder> > LimitOrderQueen;
+map<int,queue<LimitOrder> > LimitOrderQueue;
 
 
 MT4_EXPFUNC int CreateLimitOrder(int ExecuteId,char * symbol,int type,double price,double lots,int expdate){
-	::EnterCriticalSection(&_LimitOrderQueen);   // 锁定变量LimitOrderQueen
+	LockCS(&_LimitOrderQueue);   // 锁定变量LimitOrderQueue
     LimitOrder order;
 	try{
-		if ( LimitOrderQueen.count(ExecuteId) <= 0){
+		if ( LimitOrderQueue.count(ExecuteId) <= 0){
 			queue<LimitOrder> queue;	
-			LimitOrderQueen[ExecuteId] = queue;	       //   注意这里是拷贝
+			LimitOrderQueue[ExecuteId] = queue;	       //   注意这里是拷贝
 		}		
 		order.id = LimitOrderIdSeq++;
 		order.symbol = symbol;
@@ -128,33 +172,33 @@ MT4_EXPFUNC int CreateLimitOrder(int ExecuteId,char * symbol,int type,double pri
 		order.price = price;
 		order.lots = lots;
 		order.expdate = expdate;
-		LimitOrderQueen[ExecuteId].push(order);        //   注意这里是变量拷贝
+		LimitOrderQueue[ExecuteId].push(order);        //   注意这里是变量拷贝
 	}catch(...){}
-	::LeaveCriticalSection(&_LimitOrderQueen); // 释放变量LimitOrderQueen 
+	UnlockCS(&_LimitOrderQueue); // 释放变量LimitOrderQueue 
 	return(order.id);
 }
 
 MT4_EXPFUNC int GetLimitOrderCount(int ExecuteId){
 	int result;
-	::EnterCriticalSection(&_LimitOrderQueen);  // 锁定变量LimitOrderQueen
+	LockCS(&_LimitOrderQueue);  // 锁定变量LimitOrderQueue
 	try{	
-		if ( LimitOrderQueen.count(ExecuteId) > 0){ 
-			result = LimitOrderQueen[ExecuteId].size();
+		if ( LimitOrderQueue.count(ExecuteId) > 0){ 
+			result = LimitOrderQueue[ExecuteId].size();
 		}else{
 			result = 0;
 		}	
 	}catch(...){}
-	::LeaveCriticalSection(&_LimitOrderQueen);  // 释放变量LimitOrderQueen 
+	UnlockCS(&_LimitOrderQueue);  // 释放变量LimitOrderQueue 
 	return(result);
 }
 
 MT4_EXPFUNC int GetLimitOrderId(int ExecuteId){
 	int result;
-	::EnterCriticalSection(&_LimitOrderQueen);  // 锁定变量LimitOrderQueen
+	LockCS(&_LimitOrderQueue);  // 锁定变量LimitOrderQueue
 	try{	
-		if ( LimitOrderQueen.count(ExecuteId) > 0){ 
-			if(LimitOrderQueen[ExecuteId].size() > 0){
-				result = LimitOrderQueen[ExecuteId].front().id;
+		if ( LimitOrderQueue.count(ExecuteId) > 0){ 
+			if(LimitOrderQueue[ExecuteId].size() > 0){
+				result = LimitOrderQueue[ExecuteId].front().id;
 			}else{
 				result = -1;
 			}
@@ -162,18 +206,18 @@ MT4_EXPFUNC int GetLimitOrderId(int ExecuteId){
 			result = -1;
 		}
 	}catch(...){}
-	::LeaveCriticalSection(&_LimitOrderQueen);  // 释放变量LimitOrderQueen 
+	UnlockCS(&_LimitOrderQueue);  // 释放变量LimitOrderQueue 
 	return(result);
 }
 
 
 MT4_EXPFUNC char * GetLimitOrderSymbol(int ExecuteId){
 	char * result = NULL;
-	::EnterCriticalSection(&_LimitOrderQueen);  // 锁定变量LimitOrderQueen
+	LockCS(&_LimitOrderQueue);  // 锁定变量LimitOrderQueue
 	try{	
-		if ( LimitOrderQueen.count(ExecuteId) > 0){ 
-			if(LimitOrderQueen[ExecuteId].size() > 0){
-				result = (char*)LimitOrderQueen[ExecuteId].front().symbol.c_str();
+		if ( LimitOrderQueue.count(ExecuteId) > 0){ 
+			if(LimitOrderQueue[ExecuteId].size() > 0){
+				result = (char*)LimitOrderQueue[ExecuteId].front().symbol.c_str();
 			}else{
 				result = NULL;
 			}
@@ -181,17 +225,17 @@ MT4_EXPFUNC char * GetLimitOrderSymbol(int ExecuteId){
 			result = NULL;
 		}
 	}catch(...){}
-	::LeaveCriticalSection(&_LimitOrderQueen);  // 释放变量LimitOrderQueen 
+	UnlockCS(&_LimitOrderQueue);  // 释放变量LimitOrderQueue 
 	return(result);
 }
 
 MT4_EXPFUNC int GetLimitOrderType(int ExecuteId){
 	int result;
-	::EnterCriticalSection(&_LimitOrderQueen);  // 锁定变量LimitOrderQueen
+	LockCS(&_LimitOrderQueue);  // 锁定变量LimitOrderQueue
 	try{	
-		if ( LimitOrderQueen.count(ExecuteId) > 0){ 
-			if(LimitOrderQueen[ExecuteId].size() > 0){
-				result = LimitOrderQueen[ExecuteId].front().type;
+		if ( LimitOrderQueue.count(ExecuteId) > 0){ 
+			if(LimitOrderQueue[ExecuteId].size() > 0){
+				result = LimitOrderQueue[ExecuteId].front().type;
 			}else{
 				result = -1;
 			}
@@ -199,17 +243,17 @@ MT4_EXPFUNC int GetLimitOrderType(int ExecuteId){
 			result = -1;
 		}
 	}catch(...){}
-	::LeaveCriticalSection(&_LimitOrderQueen);  // 释放变量LimitOrderQueen 
+	UnlockCS(&_LimitOrderQueue);  // 释放变量LimitOrderQueue 
 	return(result);
 }
 
 MT4_EXPFUNC double GetLimitOrderPrice(int ExecuteId){
 	double result;
-	::EnterCriticalSection(&_LimitOrderQueen);  // 锁定变量LimitOrderQueen
+	LockCS(&_LimitOrderQueue);  // 锁定变量LimitOrderQueue
 	try{	
-		if ( LimitOrderQueen.count(ExecuteId) > 0){ 
-			if(LimitOrderQueen[ExecuteId].size() > 0 ){
-				result = LimitOrderQueen[ExecuteId].front().price;
+		if ( LimitOrderQueue.count(ExecuteId) > 0){ 
+			if(LimitOrderQueue[ExecuteId].size() > 0 ){
+				result = LimitOrderQueue[ExecuteId].front().price;
 			}else{
 				result = -1;
 			}
@@ -217,17 +261,17 @@ MT4_EXPFUNC double GetLimitOrderPrice(int ExecuteId){
 			result = -1;
 		}
 	}catch(...){}
-	::LeaveCriticalSection(&_LimitOrderQueen);  // 释放变量LimitOrderQueen 
+	UnlockCS(&_LimitOrderQueue);  // 释放变量LimitOrderQueue 
 	return(result);
 }
 
 MT4_EXPFUNC double GetLimitOrderLots(int ExecuteId){
 	double result;
-	::EnterCriticalSection(&_LimitOrderQueen);  // 锁定变量LimitOrderQueen
+	LockCS(&_LimitOrderQueue);  // 锁定变量LimitOrderQueue
 	try{	
-		if ( LimitOrderQueen.count(ExecuteId) > 0){ 
-			if(LimitOrderQueen[ExecuteId].size() > 0 ){
-				result = LimitOrderQueen[ExecuteId].front().lots;
+		if ( LimitOrderQueue.count(ExecuteId) > 0){ 
+			if(LimitOrderQueue[ExecuteId].size() > 0 ){
+				result = LimitOrderQueue[ExecuteId].front().lots;
 			}else{
 				result = -1;
 			}
@@ -235,7 +279,7 @@ MT4_EXPFUNC double GetLimitOrderLots(int ExecuteId){
 			result = -1;
 		}
 	}catch(...){}
-	::LeaveCriticalSection(&_LimitOrderQueen);  // 释放变量LimitOrderQueen 
+	UnlockCS(&_LimitOrderQueue);  // 释放变量LimitOrderQueue 
 	return(result);
 }
 
@@ -243,11 +287,11 @@ MT4_EXPFUNC double GetLimitOrderLots(int ExecuteId){
 
 MT4_EXPFUNC int GetLimitOrderExpdate(int ExecuteId){
 	int result;
-	::EnterCriticalSection(&_LimitOrderQueen);  // 锁定变量LimitOrderQueen
+	LockCS(&_LimitOrderQueue);  // 锁定变量LimitOrderQueue
 	try{	
-		if ( LimitOrderQueen.count(ExecuteId) > 0){ 
-			if(LimitOrderQueen[ExecuteId].size() > 0){
-				result = LimitOrderQueen[ExecuteId].front().expdate;
+		if ( LimitOrderQueue.count(ExecuteId) > 0){ 
+			if(LimitOrderQueue[ExecuteId].size() > 0){
+				result = LimitOrderQueue[ExecuteId].front().expdate;
 			}else{
 				result = -1;
 			}
@@ -255,34 +299,34 @@ MT4_EXPFUNC int GetLimitOrderExpdate(int ExecuteId){
 			result = -1;
 		}
 	}catch(...){}
-	::LeaveCriticalSection(&_LimitOrderQueen);  // 释放变量LimitOrderQueen 
+	UnlockCS(&_LimitOrderQueue);  // 释放变量LimitOrderQueue 
 	return(result);
 }
 
 
 MT4_EXPFUNC void RemoveLimitOrder(int ExecuteId){
-	::EnterCriticalSection(&_LimitOrderQueen);  // 锁定变量LimitOrderQueen
+	LockCS(&_LimitOrderQueue);  // 锁定变量LimitOrderQueue
 	try{	
-		if ( LimitOrderQueen.count(ExecuteId) > 0){ 
-			if(LimitOrderQueen[ExecuteId].size() > 0){
-				LimitOrderQueen[ExecuteId].pop();  // 删除第1个数据
+		if ( LimitOrderQueue.count(ExecuteId) > 0){ 
+			if(LimitOrderQueue[ExecuteId].size() > 0){
+				LimitOrderQueue[ExecuteId].pop();  // 删除第1个数据
 			}
 		}
 	}catch(...){}	
-	::LeaveCriticalSection(&_LimitOrderQueen);  // 释放变量LimitOrderQueen     
+	UnlockCS(&_LimitOrderQueue);  // 释放变量LimitOrderQueue     
 }
 
 MT4_EXPFUNC void TurnLimitOrder(int ExecuteId){
-	::EnterCriticalSection(&_LimitOrderQueen);  // 锁定变量LimitOrderQueen
+	LockCS(&_LimitOrderQueue);  // 锁定变量LimitOrderQueue
 	try{	
-		if ( LimitOrderQueen.count(ExecuteId) > 0){ 
-			if(LimitOrderQueen[ExecuteId].size() > 0){
-				LimitOrderQueen[ExecuteId].push(LimitOrderQueen[ExecuteId].front());  // 将队列第1数据拷贝至队列最后
-				LimitOrderQueen[ExecuteId].pop();         // 删除第1个数据
+		if ( LimitOrderQueue.count(ExecuteId) > 0){ 
+			if(LimitOrderQueue[ExecuteId].size() > 0){
+				LimitOrderQueue[ExecuteId].push(LimitOrderQueue[ExecuteId].front());  // 将队列第1数据拷贝至队列最后
+				LimitOrderQueue[ExecuteId].pop();         // 删除第1个数据
 			}
 		}
 	}catch(...){}
-	::LeaveCriticalSection(&_LimitOrderQueen);  // 释放变量LimitOrderQueen 
+	UnlockCS(&_LimitOrderQueue);  // 释放变量LimitOrderQueue 
 }
 
 
--- dlls/CppUtility/CppUtility.dsp
+++ dlls/CppUtility/CppUtility.dsp
@@ -77,7 +77,7 @@ SOURCE="$(InputPath)"
 # PROP Ignore_Export_Lib 0
 # PROP Target_Dir ""
 # ADD BASE CPP /nologo /MTd /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "CPPUTILITY_EXPORTS" /Yu"stdafx.h" /FD /GZ /c
-# ADD CPP /nologo /MTd /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "CPPUTILITY_EXPORTS" /D "_WINDLL" /FR /Yu"stdafx.h" /FD /GZ /c
+# ADD CPP /nologo /MTd /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "CPPUTILITY_EXPORTS" /D "_WINDLL" /D _WIN32_WINNT=0x0501 /FR /Yu"stdafx.h" /FD /GZ /c
 # ADD BASE MTL /nologo /D "_DEBUG" /mktyplib203 /win32
 # ADD MTL /nologo /D "_DEBUG" /mktyplib203 /win32
 # ADD BASE RSC /l 0x804 /d "_DEBUG"
